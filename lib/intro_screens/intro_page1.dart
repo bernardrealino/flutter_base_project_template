a9e7666cd3cb1a415a828218ad66cc2f36914415
Bernard <bernard.realino98@gmail.com>
2023-06-13 16:49:01 +0700

    Add Lottie Animation

--- lib/intro_screens/intro_page1.dart
+++ lib/intro_screens/intro_page1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class IntroPage1 extends StatelessWidget {
   const IntroPage1({super.key});
@@ -6,9 +7,19 @@ class IntroPage1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red[100],
-      child: Center(
-        child: Text("Page 1"),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Text("Text 1"),
+          Lottie.network(
+              'https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/B.json'),
+          Lottie.network(
+              'https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/E.json'),
+          Lottie.network(
+              'https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json'),
+          Lottie.network(
+              'https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/R.json'),
+        ],
       ),
     );
   }
