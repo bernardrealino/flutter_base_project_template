93060b2163c07f435d4e1e33c9b1f4c054554e49
Bernard <bernard.realino98@gmail.com>
2023-06-19 09:40:44 +0700

    Added Pages

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,18 +1,91 @@
 import 'package:flutter/material.dart';
+import './setting_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+  bool _isClicked = true;
+  String buttonName = "CLICK";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.greenAccent[100],
         foregroundColor: Colors.black,
+        iconTheme: IconThemeData(color: Colors.greenAccent[100]),
         title: Text("Homepage"),
       ),
       body: Center(
-        child: Text("Welcome to Homepage"),
+          child: currentIndex == 0
+              ? Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              // onPrimary: Colors.red,
+                              // primary: Colors.amber,
+                              ),
+                          onPressed: () {
+                            setState(() {
+                              buttonName = "Clicked";
+                            });
+                          },
+                          child: Text(buttonName)),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    const SettingPage(),
+                              ),
+                            );
+                          },
+                          child: Text("Move to Next Page")),
+                    ],
+                  ),
+                )
+              : GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _isClicked = !_isClicked;
+                    });
+                  },
+                  child: _isClicked
+                      ? Image.asset('images/Background.jpeg')
+                      : Image.network(
+                          "https://imgv3.fotor.com/images/blog-cover-image/part-blurry-image.jpg"),
+                )),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Setting",
+            icon: Icon(
+              Icons.settings,
+            ),
+          ),
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'splashscreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: SplashScreen(),
     );
   }
