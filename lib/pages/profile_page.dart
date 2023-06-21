c8017ce48be9e09476ac6afbbacd13e7549f229c
Bernard <bernard.realino98@gmail.com>
2023-06-21 16:39:20 +0700

    Added Card for Profile Page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -9,45 +9,50 @@ class ProfilePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Profile'),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 80,
-              foregroundImage: AssetImage('assets/profile_image.jpeg'),
-              // child: Image(image: AssetImage('assets/profile_image.jpeg')),
+      body: Center(
+        child: Card(
+          margin: EdgeInsets.all(20),
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('assets/profile_image.jpeg'),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'John Doe',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Software Developer',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text('john.doe@example.com'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.phone),
+                  title: Text('+1 (123) 456-7890'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.location_on),
+                  title: Text('New York, USA'),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            Text(
-              'John Doe',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Software Developer',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 20),
-            ListTile(
-              leading: Icon(Icons.email),
-              title: Text('john.doe@example.com'),
-            ),
-            ListTile(
-              leading: Icon(Icons.phone),
-              title: Text('+1 (123) 456-7890'),
-            ),
-            ListTile(
-              leading: Icon(Icons.location_on),
-              title: Text('New York, USA'),
-            ),
-          ],
+          ),
         ),
       ),
     );
